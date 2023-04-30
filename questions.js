7405026e1154b3226da9ffe541f87fee2e3484eb
Gilmar Alves <gilmaralves914@gmail.com>
2023-04-30 15:11:03 -0300

    questoes e respostas adicionadas

---
+++ questions.js
@@ -0,0 +1,37 @@
+export default[
+    {
+        questions: "Nome Completo: ",
+        answers: [
+            { opition: "Gilmar Junior Alves", correct: false },
+            { opition: "Gilmar Zezilia Alves", correct: true },
+            { opition: "Gilmar Junior Zezilia Alves", correct: false },
+        ],
+    },
+
+    {
+        questions: "Idade : ",
+        answers: [
+            { opition: "23", correct: true },
+            { opition: "24", correct: false },
+            { opition: "22", correct: false },
+        ],
+    },
+
+    {
+        questions: "Time do coração: ",
+        answers: [
+            { opition: "Plameiras", correct: false },
+            { opition: "São Paulo", correct: true },
+            { opition: "Corinthians", correct: false },
+        ],
+    },
+
+    {
+        questions: "Deseja atuar profissionalmente como: ",
+        answers: [
+            { opition: "Advogado", correct: false },
+            { opition: "Professor", correct: false },
+            { opition: "Programador", correct: true },
+        ],
+    }
+]
