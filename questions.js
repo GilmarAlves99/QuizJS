97e396129997f982485703c565e76b23ee06fa1c
Gilmar Alves <gilmaralves914@gmail.com>
2023-05-01 22:31:41 -0300

    Salvando respostas

--- questions.js
+++ questions.js
@@ -33,5 +33,5 @@ export default[
             { opition: "Professor", correct: false },
             { opition: "Programador", correct: true },
         ],
-    }
+    },
 ]
--- script.js
+++ script.js
@@ -11,6 +11,34 @@ import questions from "./questions.js"
 let currentIndex = 0;
 let questionsCorrect = 0;
 
+btnRestart.onclick = () => {
+    content.style.display = "flex";
+    contentFinish.style.display = "none";
+    currentIndex = 0;
+    questionsCorrect = 0;
+    loadQuestions();
+}
+
+function nextQuestion(e) {
+    if (e.target.getAttribute("data-correct") === "true") {
+        questionsCorrect++;
+    }
+    if (currentIndex < questions.length - 1) {
+        currentIndex++;
+        loadQuestions();
+    } else {
+        finish();
+    }
+}
+
+function finish() {
+    textFinish.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length}`
+    content.style.display = "none";
+    contentFinish.style.display = "flex"
+
+}
+
+
 function loadQuestions() {
     spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
     const item = questions[currentIndex];
@@ -26,6 +54,10 @@ function loadQuestions() {
         `;
         answers.appendChild(div);
     });
+
+    document.querySelectorAll(".answer").forEach((item) => {
+        item.addEventListener("click", nextQuestion)
+    })
 }
 
 loadQuestions();
